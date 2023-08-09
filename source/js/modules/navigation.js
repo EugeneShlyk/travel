81414436135d6b6e720552ce201dc40f828f6778
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2023-08-09 17:47:31 +1000

    среда вечер работа

--- source/js/modules/navigation.js
+++ source/js/modules/navigation.js
@@ -3,9 +3,10 @@ const navigationToggle = document.querySelector('.header__button-menu');
 const logo = document.querySelector('.header__logo-link');
 const phone = document.querySelector('.header__phone');
 const cardWrapper = document.querySelector('.hero-card__inner');
+const body = document.querySelector('body');
+const modal = document.querySelector('.navigation__list');
 
-
-const toggleMenuHandler = () => {
+export const toggleMenuHandler = () => {
   navigationToggle.addEventListener('click', function () {
     if (navigation.classList.contains('navigation--closed')) {
       navigation.classList.remove('navigation--closed');
@@ -18,19 +19,30 @@ const toggleMenuHandler = () => {
       phone.classList.add('header__phone--opened-menu');
       cardWrapper.classList.remove('hero-card__inner--closed-menu');
       cardWrapper.classList.add('hero-card__inner--opened-menu');
+      body.classList.add('opened-menu');
+      document.addEventListener('click', onOverlayClick);
     } else {
-      navigation.classList.add('navigation--closed');
-      navigation.classList.remove('navigation--opened');
-      navigationToggle.classList.remove('header__button-menu--opened');
-      navigationToggle.classList.add('header__button-menu--closed');
-      logo.classList.remove('header__logo-link--opened-menu');
-      logo.classList.add('header__logo-link--closed-menu');
-      phone.classList.add('header__phone--closed-menu');
-      phone.classList.remove('header__phone--opened-menu');
-      cardWrapper.classList.remove('hero-card__inner--opened-menu');
-      cardWrapper.classList.add('hero-card__inner--closed-menu');
+      closeModal();
     }
   });
 };
 
-export {toggleMenuHandler};
+const closeModal = () => {
+  navigation.classList.add('navigation--closed');
+  navigation.classList.remove('navigation--opened');
+  navigationToggle.classList.remove('header__button-menu--opened');
+  navigationToggle.classList.add('header__button-menu--closed');
+  logo.classList.remove('header__logo-link--opened-menu');
+  logo.classList.add('header__logo-link--closed-menu');
+  phone.classList.add('header__phone--closed-menu');
+  phone.classList.remove('header__phone--opened-menu');
+  cardWrapper.classList.remove('hero-card__inner--opened-menu');
+  cardWrapper.classList.add('hero-card__inner--closed-menu');
+  body.classList.remove('opened-menu');
+};
+
+const onOverlayClick = (evt) => {
+  if (evt.target === navigation && evt.target !== modal) {
+    closeModal();
+  }
+};
