8559eb8423566a93f451e2e42fa409662710c5c6
Evgene Shlyk <serpupstars@gmail.com>
2023-08-12 14:37:39 +1000

    14-36

--- source/js/modules/navigation.js
+++ source/js/modules/navigation.js
@@ -2,10 +2,16 @@ const navigation = document.querySelector('.navigation');
 const navigationToggle = document.querySelector('.header__button-menu');
 const logo = document.querySelector('.header__logo-link');
 const phone = document.querySelector('.header__phone');
-const cardWrapper = document.querySelector('.hero-card__inner');
 const body = document.querySelector('body');
 const modal = document.querySelector('.navigation__list');
 const menuLink = document.querySelector('.navigation__list-item-link');
+
+// const cardWrapper = document.querySelector('.hero-card__inner');
+// cardWrapper.classList.remove('hero-card__inner--closed-menu');
+// cardWrapper.classList.add('hero-card__inner--opened-menu');
+// cardWrapper.classList.remove('hero-card__inner--opened-menu');
+// cardWrapper.classList.add('hero-card__inner--closed-menu');
+
 
 export const toggleMenuHandler = () => {
   navigationToggle.addEventListener('click', () => {
@@ -18,8 +24,6 @@ export const toggleMenuHandler = () => {
       logo.classList.add('header__logo-link--opened-menu');
       phone.classList.remove('header__phone--closed-menu');
       phone.classList.add('header__phone--opened-menu');
-      cardWrapper.classList.remove('hero-card__inner--closed-menu');
-      cardWrapper.classList.add('hero-card__inner--opened-menu');
       body.classList.add('opened-menu');
       document.addEventListener('click', onOverlayClick);
       menuLink.addEventListener('click', onMenuLinksClick);
@@ -39,8 +43,6 @@ const closeModal = () => {
   logo.classList.add('header__logo-link--closed-menu');
   phone.classList.add('header__phone--closed-menu');
   phone.classList.remove('header__phone--opened-menu');
-  cardWrapper.classList.remove('hero-card__inner--opened-menu');
-  cardWrapper.classList.add('hero-card__inner--closed-menu');
   body.classList.remove('opened-menu');
 };
 
