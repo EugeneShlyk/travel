153bde5ec94be31d39760d903e2db7bc45886f32
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2023-08-15 15:45:21 +1000

    пнд

--- source/js/modules/advantages-slider.js
+++ source/js/modules/advantages-slider.js
@@ -3,7 +3,7 @@ const slider = document.querySelector('.advantages__slider-wrapper');
 
 
 export const initAdvantagesSlider = () =>
-  slider &&
+  window.matchMedia('(min-width: 1200px)').matches && slider &&
   new Swiper('.advantages__slider-wrapper', {
     navigation: {
       nextEl: '.advantages-button--to-right',
@@ -11,18 +11,6 @@ export const initAdvantagesSlider = () =>
     },
     simulateTouch: false,
     initialSlide: 1,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 18,
-      },
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-    },
+    slidesPerView: 3,
+    spaceBetween: 30,
   });
