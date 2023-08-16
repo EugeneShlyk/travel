6e8e5b4c4c0f76e2e0374b000ed2ed7c8c49c6b1
Evgene Shlyk <serpupstars@gmail.com>
2023-08-17 07:26:07 +1000

    четверг утро, заканчивает галерею, бэкграунд для about us на таблет нужно подправить.

--- source/js/modules/gallery-slider.js
+++ source/js/modules/gallery-slider.js
@@ -3,14 +3,20 @@ const slider = document.querySelector('.gallery__slider-wrapper');
 
 
 export const initGallerySlider = () =>
-  window.matchMedia('(min-width: 1200px)').matches && slider &&
-  new Swiper('.gallery__slider-wrapper', {
+  slider && new Swiper('.gallery__slider-wrapper', {
     navigation: {
       nextEl: '.gallery__button--to-right',
       prevEl: '.gallery__button--to-left',
     },
     simulateTouch: false,
-    spaceBetween: 5,
-    slidesPerView: 'auto',
     freeMode: true,
+    slidesPerView: 'auto',
+    breakpoints: {
+      320: {
+        spaceBetween: 3,
+      },
+      768: {
+        spaceBetween: 5,
+      },
+    },
   });
