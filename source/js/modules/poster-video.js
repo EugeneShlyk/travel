8bc6da89d902ae2925f6cc4cf4881603b33788af
Evgene Shlyk <serpupstars@gmail.com>
2023-08-22 07:25:00 +1000

    вторник 22 утро

--- source/js/modules/poster-video.js
+++ source/js/modules/poster-video.js
@@ -2,8 +2,8 @@ const video = document.querySelector('.hero-card__video');
 
 export const changePoster = () => {
   if (window.matchMedia('(min-width: 1200px)').matches) {
-    video.poster = './../../img/hero/caucasus/hero-video-poster.jpg';
+    video.poster = './../../img/hero/caucasus/hero-video-poster.webp';
   } if (window.matchMedia('(max-width: 1199px)').matches) {
-    video.poster = './../../img/hero/caucasus/hero-video-poster-tablet.jpg';
+    video.poster = './../../img/hero/caucasus/hero-video-poster-tablet.webp';
   }
 };
