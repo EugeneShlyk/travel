aa8d8921b89ed5ec434ca4c000d7017f514118a7
Evgene Shlyk <serpupstars@gmail.com>
2023-08-25 07:25:22 +1000

    птн. утро

---
+++ source/js/modules/add-media-hero.js
@@ -0,0 +1,20 @@
+const slides = document.querySelectorAll('.hero-card');
+const video = slide.querySelector('.hero-card__video');
+const audio = slide.querySelector('.hero-card__inner-container-audio');
+
+// export const addMediaSrc = () => {
+//   slides.forEach((slide) => {
+//     let childSlides = slide.childNodes;
+//     childSlides.forEach((childSlide) => {
+//
+//     })
+//   });
+// };
+
+export const addMediaSrc = () => {
+
+};
+
+
+// проверяет объект на свойство
+// obj.hasOwnProperty(prop)
